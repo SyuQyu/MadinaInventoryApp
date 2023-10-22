435e8c39262240496533f8a3e97fc2aaed79fc09
Pandu <104115882+SyuQyu@users.noreply.github.com>
2023-10-23 00:33:14 +0700

    feat: penjualan

--- src/pages/Penjualan.tsx
+++ src/pages/Penjualan.tsx
@@ -1,24 +1,38 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonRouterLink, IonTitle, IonToolbar } from '@ionic/react';
 import { ExploreContainer } from '../components';
 import '../theme/pages/Tab2.css';
-
+import { IoLogInOutline } from 'react-icons/io5'
 const Tab2: React.FC = () => {
   return (
-    <>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen={false}>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
-      </IonContent>
-    </>
+    <IonContent fullscreen={false}>
+      <div className='md:px-10 md:py-10 px-2 py-5'>
+        <header className='mb-6'>
+          <h1 className='text-2xl font-extrabold text-[#280822]'>Penjualan</h1>
+        </header>
+        <div className='w-full h-full flex md:flex-row flex-col justify-start items-start gap-6'>
+          <IonRouterLink routerLink="/home" className='text-white w-full md:p-6 p-4 bg-[#280822] rounded-xl'>
+            <div className='w-full flex flex-col justify-between text-left items-start py-5 gap-4'>
+              <h1 className='font-bold text-4xl'>
+                Stock In <IoLogInOutline className='ml-5 float-right w-10 h-10' />
+              </h1>
+              <p className='pr-10'>
+                Pilih Item dan inputkan kedalam untuk merekam history barang masuk
+              </p>
+            </div>
+          </IonRouterLink>
+          <IonRouterLink routerLink="/home" className='text-white w-full md:p-6 p-4 bg-[#280822] rounded-xl'>
+            <div className='w-full flex flex-col justify-between text-right items-end py-5 gap-4'>
+              <h1 className='font-bold text-4xl'>
+                Stock Out <IoLogInOutline className='ml-5 float-right w-10 h-10' />
+              </h1>
+              <p className='pl-10'>
+                Pilih Item dan inputkan kedalam untuk merekam history barang keluar
+              </p>
+            </div>
+          </IonRouterLink>
+        </div>
+      </div>
+    </IonContent>
   );
 };
 
