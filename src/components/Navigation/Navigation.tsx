243f61baa0e3d4dd032c46be18b6c725bba2b60a
Pandu <104115882+SyuQyu@users.noreply.github.com>
2023-11-04 13:40:22 +0700

    feat: presistent data

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -4,11 +4,19 @@ import { IonReactRouter } from '@ionic/react-router';
 
 import { Route, Redirect } from 'react-router';
 import { HiOutlineHome, HiOutlineQueueList, HiOutlineShoppingBag } from "react-icons/hi2";
+import { BiLogOut } from "react-icons/bi";
 import { GoHistory } from "react-icons/go";
 import { TbTransfer } from "react-icons/tb";
 import useTransactionStore from '../../context/transaksi';
+import useAuth from '../../context/auth';
 function Tabs() {
     const { deleteAllSelectedItems } = useTransactionStore();
+    const { isLoggedIn, token, dataUser, logout } = useAuth();
+
+    const logoutUser = () => {
+        logout();
+        deleteAllSelectedItems();
+    }
     return (
         <>
             <ul className="flex text-sm font-medium text-center text-gray-500 shadow h-full p-2 md:p-5 bg-[#280822]">
@@ -34,6 +42,12 @@ function Tabs() {
                     <IonRouterLink router-direction={'forward'} onClick={() => deleteAllSelectedItems()} routerLink="/history" className='flex flex-col gap-10 justify-between items-center' style={{ color: 'white' }}>
                         <GoHistory className='w-full h-5 sm:h-6 md:h-8 mb-2' />
                         <p className='text-xs md:text-md font-bold text-white'>History</p>
+                    </IonRouterLink>
+                </li>
+                <li className="w-full">
+                    <IonRouterLink onClick={() => logoutUser()} routerLink="/login" className='flex flex-col gap-10 justify-between items-center' style={{ color: 'white' }}>
+                        <BiLogOut className='w-full h-5 sm:h-6 md:h-8 mb-2' />
+                        <p className='text-xs md:text-md font-bold text-white'>Logout</p>
                     </IonRouterLink>
                 </li>
             </ul>
--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-
+import { persist } from "zustand/middleware";
 type AuthState = {
     token: string | null;
     isLoggedIn: boolean;
@@ -8,37 +8,76 @@ type AuthState = {
     logout: () => void;
 };
 
-const useAuth = create<AuthState>((set) => ({
-    token: sessionStorage.getItem('token'),
-    isLoggedIn: localStorage.getItem('isLoggedIn') === 'true' ? true : false,
-    dataUser: sessionStorage.getItem('dataUser') ? JSON.parse(sessionStorage.getItem('dataUser') as string) : null,
-    login: async (email, password) => {
-        const response = await fetch('https://inventory-app.kaladwipa.com/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ email, password }),
-        });
+// const useAuth = create<AuthState>((set) => ({
+    
+//     token: sessionStorage.getItem('token'),
+//     isLoggedIn: localStorage.getItem('isLoggedIn') === 'true' ? true : false,
+//     dataUser: sessionStorage.getItem('dataUser') ? JSON.parse(sessionStorage.getItem('dataUser') as string) : null,
+//     login: async (email, password) => {
+//         const response = await fetch('https://inventory-app.kaladwipa.com/login', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//             body: JSON.stringify({ email, password }),
+//         });
         
-        console.log("called");
-        const { data } = await response.json();
-        const token = data.token.token;
-        if (response.ok) {
-            console.log(token);
-            localStorage.setItem('isLoggedIn', 'true');
-            sessionStorage.setItem('token', token);
-            console.log(data.user)
-            sessionStorage.setItem('dataUser', JSON.stringify(data.user));
-            set({ token, isLoggedIn: true, dataUser: data.user });
-        } else {
-            throw new Error('Login failed');
+//         console.log("called");
+//         const { data } = await response.json();
+//         const token = data.token.token;
+//         if (response.ok) {
+//             console.log(token);
+//             localStorage.setItem('isLoggedIn', 'true');
+//             sessionStorage.setItem('token', token);
+//             console.log(data.user)
+//             sessionStorage.setItem('dataUser', JSON.stringify(data.user));
+//             set({ token, isLoggedIn: true, dataUser: data.user });
+//         } else {
+//             throw new Error('Login failed');
+//         }
+//     },
+//     logout: () => {
+//         sessionStorage.removeItem('token');
+//         set({ token: null, isLoggedIn: false });
+//     },
+// }));
+const useAuth = create<AuthState>() (
+    persist(
+        (set) => ({
+            token: null,
+            isLoggedIn: false,
+            dataUser: null,
+            login: async (email, password) => {
+                const response = await fetch('https://inventory-app.kaladwipa.com/login', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ email, password }),
+                });
+                
+                const { data } = await response.json();
+                const token = await data?.token?.token;
+                console.log("called", email, password, response.ok, data, token);
+                if (response.ok) {
+                    // console.log(token);
+                    // localStorage.setItem('isLoggedIn', 'true');
+                    // sessionStorage.setItem('token', token);
+                    // console.log(data.user)
+                    // sessionStorage.setItem('dataUser', JSON.stringify(data.user));
+                    set({ token, isLoggedIn: true, dataUser: data.user });
+                } else {
+                    throw new Error('Login failed');
+                }
+            },
+            logout: () => {
+                // sessionStorage.removeItem('token');
+                set({ token: null, isLoggedIn: false, dataUser: null });
+            },
+        }),{
+            name: "auth-storage",
         }
-    },
-    logout: () => {
-        sessionStorage.removeItem('token');
-        set({ token: null, isLoggedIn: false });
-    },
-}));
+    )
+);
 
 export default useAuth;
